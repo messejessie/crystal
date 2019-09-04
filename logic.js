a7de3b8cb788d0d7a85dd4b0ce18369ea12693d4
messejessie <messejessie77@gmail.com>
2019-09-04 12:49:52 -0400

    start game and starting of win loss counts functions

--- logic.js
+++ logic.js
@@ -30,6 +30,48 @@ let lossCount = 0;
 
 //functionality 
 
-getRandom => (min, max){
-    return Math.floor(Math.random()* (max - min + 1)) + min;
+getRandom = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+//Start and Restart of game: 
+startGame = () => {
+    //current Score
+    currentScore = 0
+
+    //target score
+    targetScore = getRandom(19, 120);
+
+    //set values
+    crystal.blue.value = getRandom(1, 12);
+    crystal.red.value = getRandom(1, 12);
+    crystal.green.value = getRandom(1, 12);
+    crystal.yellow.value = getRandom(1, 12);
+
+    //HTML changes: 
+    $('#your-score').text(currentScore);
+    $('#target-score').text(targetScore);
+
+    // testing 
+    console.log("--------------------");
+    console.log("Target Score: " + targetScore);
+    console.log("Blue: " + crystal.blue.value + " | Green: " + crystal.green.value + " | Red: " + crystal.red.value +
+        " | Yellow: " + crystal.yellow.value);
+
+    console.log("--------------------");
+};
+
+//check win
+
+checkwin = () => {
+    //check if current score is larger than target score
+    if (currentScore > targetScore) {
+        alert('You Lose!')
+        console.log('You Lose')
+        $('#loss-count').text(lossCount);
+        //restart
+        startGame();
+    }
+
+
 }
